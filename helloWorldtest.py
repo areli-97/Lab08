cf9e60afda30ec2d0ac2e9063b0390836be23d8c
areli-97 <129007535+areli-97@users.noreply.github.com>
2023-03-26 17:56:53 -0400

    Welcome to facebook changes

---
+++ helloWorldtest.py
@@ -0,0 +1,7 @@
+userName = input("Hello! Please enter your facebook username! ")
+userPassword = input("Hello! Please enter your facebook password! ")
+
+if(userName.lower() == "areli" and userPassword == "pizza123"):
+    print("Welcome to facebook!")
+else:
+    print("Wrong credentials, try again!")
