a35c74db55c17d9c1452cdc67848b4a1658e620c
areli-97 <129007535+areli-97@users.noreply.github.com>
2023-03-26 17:50:58 -0400

    new changes, 2

--- helloWorld.py
+++ helloWorld.py
@@ -1,1 +1,6 @@
-print ("Hello World, my name is Areli Almaraz and I love Python!")
+#print ("Hello World, my name is Areli Almaraz and I love Python!")
+
+userName = input ("Hello! Please enter your name here!")
+
+print ("Hello", userName, "nice to meet you")
+
